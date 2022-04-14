6ccc7b0825969f8c57688fc1bbe77daf4fd2b5c8
nvegaBIRT <nvega@birt.eus>
2022-04-14 17:39:05 +0200

    users update and delete added

--- index.js
+++ index.js
@@ -27,6 +27,16 @@ app.get('/api/users', (req,res) => {
     res.send(users)
 })
 
+app.get('/api/users/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const user = users.find(user => user.id === id)
+    if(user){
+        res.json(user)
+    }else{
+        res.status(404).end()
+    }
+})
+
 app.post('/api/users', (req, res) => {
     const user = req.body
     
@@ -40,15 +50,40 @@ app.post('/api/users', (req, res) => {
     const maxId = Math.max(...ids)
 
     const newUser = {
+        id: maxId + 1,
         username: user.username,
         email: user.email,
         password: user.password,
-        id: maxId + 1
     }
 
     users = [...users, newUser]
 
     res.status(201).json(newUser) //201 created
+})
+
+app.put('/api/users/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const indexOfUser = users.map(user => user.id).indexOf(id)
+    const user = req.body
+    console.log(users[indexOfUser])
+
+    if(indexOfUser===-1){
+        return res.status(400).json({
+            error: 'note.content is missing'
+        })
+    }
+
+    users[indexOfUser].username = user.username
+    users[indexOfUser].email = user.email
+    users[indexOfUser].password = user.password
+
+    res.status(201).json(users[indexOfUser]) //201 created
+})
+
+app.delete('/api/users/:id', (req, res) => {
+    const id = Number(req.params.id)
+    users = users.filter(user => user.id !== id)
+    res.status(204).end()
 })
 
 
